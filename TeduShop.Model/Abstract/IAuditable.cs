9beebc95f07779a3191c0b8215fe9295096385cd
HiepND <ndhiep.soict@gmail.com>
2018-02-20 11:02:37 +0700

    bai 6, xay dung Data

---
+++ TeduShop.Model/Abstract/IAuditable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TeduShop.Model.Abstract
+{
+    public interface IAuditable
+    {
+        DateTime? CreatedDate { get; set; }
+        string CreateBy { get; set; }
+        DateTime? UpdatedDate { set; get; }
+        string UpdateBy { set; get; }
+        string MetaKeyword { set; get; }
+        string MetaDescription { set; get; }
+        bool Status { set; get; }
+    }
+
+
+}
